a4e1905a3319ee37d8302f782ca9fbc911f02f41
SnehansuBehera <snehansbehera080903@gmail.com>
2024-05-19 21:27:51 +0530

    project-setup

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+
+import dotenv from "dotenv";
+// import path from "path"
+import cookieParser from "cookie-parser";
+import express from "express"
+import connectdb from "./config/db.js";
+import router from "./routes/userRoutes.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+connectdb();
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', router);
+
+
+app.listen(port, () => {
+    console.log("Server running...");
+})
