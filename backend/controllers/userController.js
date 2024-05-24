781b52cc8e856187193709bc1b68ed54f0714417
SnehansuBehera <snehansbehera080903@gmail.com>
2024-05-24 22:43:48 +0530

    user-management

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,7 +51,12 @@ const loginUser = asyncHandler(async (req, res) => {
 
                 }
             )
+        } else {
+            res.status(400).send("Wrong credential");
+
         }
+    } else {
+        res.status(400).send("User does not exists")
     }
 })
 
@@ -64,9 +69,63 @@ const logoutUser = asyncHandler(async (req, res) => {
         message: "Logged out successfully"
     })
 })
-const getAllUsers = asyncHandler(async (req, res,) => {
+const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find({});
     res.status(201).json(users);
 })
 
-export { createUser, loginUser, logoutUser, getAllUsers };
+const getProfile = asyncHandler(async (req, res) => {
+    const user = await User.findOne(req.user._id);
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        })
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+})
+
+const updateProfile = asyncHandler(async (req, res) => {
+    const user = await User.findOne(req.user._id);
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            const updatedhashedPassword = await bcrypt.hash(req.body.password, salt);
+            user.password = updatedhashedPassword || user.password;
+        }
+        const updatedUser = await user.save();
+        res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+            password: updatedUser.password,
+            isAdmin: updatedUser.isAdmin
+        })
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+})
+
+const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        if (user.isAdmin) {
+            res.status(404);
+            throw new Error("Cannot delete the admin");
+        }
+        await User.deleteOne({ _id: user._id });
+        res.status(201).json({
+            message: "User removed"
+        })
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+})
+export { createUser, loginUser, logoutUser, getAllUsers, getProfile, updateProfile, deleteUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, loginUser, logoutUser, getAllUsers } from "../controllers/userController.js";
+import { createUser, loginUser, logoutUser, getAllUsers, getProfile, updateProfile, deleteUser } from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 
@@ -7,5 +7,8 @@ const router = express.Router();
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.route('/auth').post(loginUser);
 router.post('/logout', logoutUser);
+router.route('/profile').get(authenticate, getProfile).put(authenticate, updateProfile);
+router.route('/:id').delete(authenticate, authorizeAdmin, deleteUser);
+
 
 export default router;
