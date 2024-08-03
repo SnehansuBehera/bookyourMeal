648c94ea8d4f97058e06e37f51548019905405ad
SnehansuBehera <snehansbehera080903@gmail.com>
2024-08-03 22:45:23 +0530

    fixes

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "https://bookmymeals.netlify.app/"
-export const USERS_URL = "https://bookmymeals.netlify.app/api/users"
-export const CATEGORY_URL = "https://bookmymeals.netlify.app/api/category"
-export const PRODUCT_URL = "https://bookmymeals.netlify.app/api/product"
-export const UPLOAD_URL = "https://bookmymeals.netlify.app/api/uploads";
-export const ORDERS_URL = "https://bookmymeals.netlify.app/api/orders";
-export const PAYPAL_URL = "https://bookmymeals.netlify.app/api/config/paypal";
+export const BASE_URL = " "
+export const USERS_URL = "/api/users"
+export const CATEGORY_URL = "/api/category"
+export const PRODUCT_URL = "/api/product"
+export const UPLOAD_URL = "/api/uploads";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
