73e40b1afd23bf2c1d0f55201707a86ca3c42ea6
SnehansuBehera <snehansbehera080903@gmail.com>
2024-08-17 18:30:25 +0530

    final-init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://bookmymeals.netlify.app",
-      "/uploads/": "https://bookmymeals.netlify.app"
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000"
     }
   },
   build: {
